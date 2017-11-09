75861ad3a4cd8d41d098c0066365726ae9291ca8
Steve Engledow <steve@offend.me.uk>
2017-11-09 17:05:06 +0000

    Add working template

--- lambda.py
+++ lambda.py
@@ -2,6 +2,19 @@
 The handler
 """
 
+import json
+
 def handler(event, context):
-    print(event)
-    print(context)
+    print(json.dumps(event, indent=4))
+
+    print("USER:", event["requestContext"]["authorizer"]["claims"]["cognito:username"])
+
+    return {
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps({
+            "Doing": "a thing",
+        }),
+    }
